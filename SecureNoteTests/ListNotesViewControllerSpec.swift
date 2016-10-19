6eff0d8470ee2bf7a3509702e4dddd28f7f2e938
Karan Krishnani <krkrishn@thoughtworks.com>
2016-10-19 08:53:24 -0400

    Add Carthage, add Quick and Nimble for test target.

---
+++ SecureNoteTests/ListNotesViewControllerSpec.swift
@@ -0,0 +1,20 @@
+//
+//  ListNotesViewControllerSpec.swift
+//  SecureNote
+//
+//  Created by Karan Krishnani on 10/18/16.
+//  Copyright © 2016 ThoughtWorks. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+class ListNotesViewControllerSpec: QuickSpec {
+    override func spec() {
+        describe("quick test") {
+            it("should pass") {
+                expect(true).to(beTrue())
+            }
+        }
+    }
+}
